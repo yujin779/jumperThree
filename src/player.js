1147212340cead4b7b49ac8fe4b976674ede7e22
yujin779 <51077021+yujin779@users.noreply.github.com>
2021-01-05 20:48:19 +0900

    importCannon

--- src/player.js
+++ src/player.js
@@ -1,15 +1,13 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import CANNON from "cannon";
 
 import dino from "../assets/gltf/dino.glb";
 
 export class Player {
   constructor(scene) {
-    console.log("cnst");
     //glTFの読み込み
     const loader = new GLTFLoader();
-    console.log(loader);
-    console.log(dino);
     loader.load(dino, (data) => {
       this.object = data.scene;
       // //拡大
@@ -17,5 +15,8 @@ export class Player {
       // this.object.scale.set(scale, scale, scale);
       scene.add(this.object);
     });
+
+    this.world = new CANNON.World();
+    this.world.gravity.set(0, -10, 0);
   }
 }
