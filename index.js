80dadf2912b83ec875346d1e6433fbfe66188c33
Tsunayoshi Arai <yujin779@gmail.com>
2021-01-05 20:13:38 +0900

    dino.glbを描画

--- index.js
+++ index.js
@@ -1,74 +1,96 @@
-import {
-  Scene,
-  Color,
-  Mesh,
-  MeshNormalMaterial,
-  BoxBufferGeometry,
-  PerspectiveCamera,
-  WebGLRenderer
-} from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import Stats from 'stats.js'
-
-class Example {
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import Stats from "stats.js";
+import dino from "./assets/gltf/dino.glb";
+class Jumper {
   constructor() {
-    this.init = this.init.bind(this)
-    this.animate = this.animate.bind(this)
-    this.onWindowResize = this.onWindowResize.bind(this)
+    this.init = this.init.bind(this);
+    this.animate = this.animate.bind(this);
+    this.onWindowResize = this.onWindowResize.bind(this);
 
-    this.init()
+    this.init();
+    this.defaultLigts();
+    this.load();
+  }
+
+  defaultLigts() {
+    const light = new THREE.DirectionalLight(0xffffff, 0.75);
+    light.intensity = 1; // 光の強さ
+    light.position.set(1.5, 3, 2.5);
+    this.scene.add(light);
+    const hemiLight = new THREE.HemisphereLight(0x888888, 0x000000, 1);
+    this.scene.add(hemiLight);
+    //座標軸を表示 x=red y=green z=blue
+    var axis = new THREE.AxesHelper(1000);
+    this.scene.add(axis);
+  }
+
+  load() {
+    //glTFの読み込み
+    const loader = new GLTFLoader();
+    console.log("start");
+    console.log(loader);
+    console.log(dino);
+    loader.load(dino, (data) => {
+      this.object = data.scene;
+      // //拡大
+      // let scale = 5;
+      // this.object.scale.set(scale, scale, scale);
+      this.scene.add(this.object);
+    });
   }
 
   init() {
-    this.aspect = window.innerWidth / window.innerHeight
-    this.camera = new PerspectiveCamera(50, this.aspect, 1, 1000)
-    this.camera.position.z = 700
+    this.aspect = window.innerWidth / window.innerHeight;
+    this.camera = new THREE.PerspectiveCamera(50, this.aspect, 1, 1000);
+    this.camera.position.z = 20;
 
-    this.controls = new OrbitControls(this.camera)
+    this.controls = new OrbitControls(this.camera);
 
-    this.geometry = new BoxBufferGeometry(200, 200, 200)
-    this.material = new MeshNormalMaterial()
-    this.mesh = new Mesh(this.geometry, this.material)
+    // this.geometry = new THREE.BoxBufferGeometry(200, 200, 200);
+    // this.material = new THREE.MeshNormalMaterial();
+    // this.mesh = new THREE.Mesh(this.geometry, this.material);
 
-    this.scene = new Scene()
-    this.scene.background = new Color('#191919')
-    this.scene.add(this.mesh)
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color("#191919");
+    // this.scene.add(this.mesh);
 
-    this.renderer = new WebGLRenderer({
-      powerPreference: 'high-performance',
-      antialias: true
-    })
+    this.renderer = new THREE.WebGLRenderer({
+      powerPreference: "high-performance",
+      antialias: true,
+    });
 
-    document.body.appendChild(this.renderer.domElement)
-    window.addEventListener('resize', this.onWindowResize)
+    document.body.appendChild(this.renderer.domElement);
+    window.addEventListener("resize", this.onWindowResize);
 
-    this.stats = new Stats()
-    document.body.appendChild(this.stats.dom)
+    this.stats = new Stats();
+    document.body.appendChild(this.stats.dom);
 
-    this.renderer.setPixelRatio(window.devicePixelRatio)
-    this.renderer.setSize(window.innerWidth, window.innerHeight)
-    this.renderer.render(this.scene, this.camera)
-    this.renderer.setAnimationLoop(this.animate)
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.render(this.scene, this.camera);
+    this.renderer.setAnimationLoop(this.animate);
   }
 
   animate() {
-    this.stats.begin()
+    this.stats.begin();
 
-    this.mesh.rotation.x += 0.005
-    this.mesh.rotation.y += 0.001
+    // this.mesh.rotation.x += 0.005;
+    // this.mesh.rotation.y += 0.001;
 
-    this.controls.update()
-    this.renderer.render(this.scene, this.camera)
+    this.controls.update();
+    this.renderer.render(this.scene, this.camera);
 
-    this.stats.end()
+    this.stats.end();
   }
 
   onWindowResize() {
-    this.camera.aspect = window.innerWidth / window.innerHeight
-    this.camera.updateProjectionMatrix()
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
 
-    this.renderer.setSize(window.innerWidth, window.innerHeight)
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
   }
 }
 
-new Example()
+new Jumper();
