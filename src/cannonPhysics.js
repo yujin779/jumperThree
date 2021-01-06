2b9be274fa73acdad422c85ccbbb8aac3deddb4d
Tsunayoshi Arai <yujin779@gmail.com>
2021-01-07 05:25:27 +0900

    重力変更

--- src/cannonPhysics.js
+++ src/cannonPhysics.js
@@ -7,7 +7,7 @@ import dino from "../assets/gltf/dino.glb";
 export class CannonPhysics {
   constructor(scene) {
     this.world = new CANNON.World();
-    this.world.gravity.set(0, -10, 0);
+    this.world.gravity.set(0, -30, 0);
     this.world.broadphase = new CANNON.NaiveBroadphase(); //ぶつかっている可能性のあるオブジェクト同士を見つける
     this.world.solver.iterations = 8; //反復計算回数
     this.world.solver.tolerance = 0.1; //許容値
